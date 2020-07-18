1c0f59b019827ec503f1067a0350dd7ddee75997
sachii19 <67509437+sachii19@users.noreply.github.com>
2020-07-18 19:23:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+document.getElementById("time").innerHTML = Date();
+
+function copy() {  
+      alert("LOL ! You can't Copy")
+    }
+    
+    function summer() {   
+         document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')";
+         document.body.style.backgroundRepeat = "no-repeat";   
+          document.body.style.backgroundSize = "100%";
+          alert("Hey, It is Summer. Its hot and the best time to take a swim and eat ice-creams." )
+        }
+        function winter () {   
+            document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+            document.body.style.backgroundRepeat = "no-repeat";   
+             document.body.style.backgroundSize = "100%";
+             alert("Hey, It is Winter. Its cold and snowing, making snowmans and sledging sounds good." )
+           }
+           function autumn () {   
+            document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+            document.body.style.backgroundRepeat = "no-repeat";   
+             document.body.style.backgroundSize = "100%";
+             alert("Hey, It is Autumn. This is the time of the year when leaves change colours and fall." )
+           }
+           function spring () {   
+            document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+            document.body.style.backgroundRepeat = "no-repeat";   
+             document.body.style.backgroundSize = "100%";
+             alert("Hey, It is Spring. This is the time of the year when the flowers are blooming into beautiful colors." )
+           }
